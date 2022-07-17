9f43e2b327552c3203ca3a6ead39edfec7288b1e
Nant <rajaonaanjasave@gmail.com>
2022-07-17 21:42:44 +0300

    Filter list and add image and date

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -21,8 +21,9 @@ class ArticlesController extends AbstractController
     public function index(): Response
     {
         //liste de tous les articles
-        $articles = $this->getDoctrine()->getRepository(Articles::class)->findAll();
+        $articles = $this->getDoctrine()->getRepository(Articles::class)->findBy([], ['created_at'=>'desc']);
         // $articlesR = $ar->findAll();
+        dump($articles);
         return $this->render('articles/index.html.twig', [
             'title' => 'Liste des articles',
             'articles' => $articles,
